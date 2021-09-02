38943ab7b25289d893a71e4f7a0dbca1170bea46
DMurray-Chadfield <82474724+DMurray-Chadfield@users.noreply.github.com>
2021-09-02 12:57:03 +0100

    Create index.php

---
+++ html_php_calculator/index.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+<!--Your code goes here-->
+<h1>Addition</h1>
+<form action="addition.php" method="GET">
+  <input type="number" name="a1"></input>
+  <input type="number" name="a2"></input>
+  <input type="submit"></input>
+</form>
+<h1>Division</h1>
+<form action="division.php" method="GET">
+  <input type="number" name="d1"></input>
+  <input type="number" name="d2"></input>
+  <input type="submit"></input>
+</form>
+<a href="index.php">Reset</a>
+</body>
+</html>
