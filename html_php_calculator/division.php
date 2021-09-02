511d1ceff6a00ce25e59a81e2975d8799da8f163
DMurray-Chadfield <82474724+DMurray-Chadfield@users.noreply.github.com>
2021-09-02 12:57:43 +0100

    Create division.php

---
+++ html_php_calculator/division.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<!--Your code goes here-->
+Your numbers were: <?= $_GET["d1"], " and ", $_GET["d2"]; ?>
+<br>
+The answer is: <?= $_GET["d1"] / $_GET["d2"]; ?>
+<br>
+<a href="index.php">Reset</a>
+</body>
+</html>
