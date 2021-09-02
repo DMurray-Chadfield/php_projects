3a0a4c85f10fcbfcacfe724c0ca0bfbc02a0a17c
DMurray-Chadfield <82474724+DMurray-Chadfield@users.noreply.github.com>
2021-09-02 12:57:25 +0100

    Create addition.php

---
+++ html_php_calculator/addition.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<!--Your code goes here-->
+Your numbers were: <?= $_GET["a1"], " and ", $_GET["a2"]; ?>
+<br>
+The answer is: <?= $_GET["a1"] + $_GET["a2"]; ?>
+<br>
+<a href="index.php">Reset</a>
+</body>
+</html>
